8d50fadaccd8fb20da9438b1d0b89c876430ba0e
ExaInsanity <nicolaiandorfer@outlook.com>
2021-09-04 14:55:06 +0200

    add: DiscordSnowflake implementation

---
+++ Starnight/Internal/Entities/DiscordSnowflake.cs
@@ -0,0 +1,117 @@
+namespace Starnight.Internal.Entities;
+
+using System.Text.Json.Serialization;
+
+using Starnight.Internal.Utils;
+
+/// <summary>
+/// Base class for all Snowflake objects. Used chiefly for REST purposes and basic abstraction. Do not use directly.
+/// One ID can only refer to one specific object, Discord ensures that.
+/// </summary>
+public class DiscordSnowflake : IConvertible, IEquatable<DiscordSnowflake>
+{
+	/// <summary>
+	/// Snowflake Identifier of this object.
+	/// </summary>
+	[JsonPropertyName("id")]
+	[JsonNumberHandling(JsonNumberHandling.AllowReadingFromString)]
+	public Int64 Id { get; init; }
+
+	/// <summary>
+	/// Creation date of this object.
+	/// </summary>
+	[JsonIgnore]
+	public DateTimeOffset Creation => this.Id.GetSnowflakeTime();
+
+	/// <summary>
+	/// Gets whether this Snowflake equals another.
+	/// </summary>
+	/// <returns><see langword="false"/> if one is null or the objects do not match</returns>
+	public Boolean Equals(DiscordSnowflake? other)
+		=> (this is null || other is not null) && this.Id == other!.Id;
+
+	public TypeCode GetTypeCode() => TypeCode.Int64;
+
+	/// <summary>
+	/// Always throws InvalidCastException.
+	/// </summary>
+	public Boolean ToBoolean(IFormatProvider? provider) => throw new InvalidCastException();
+
+	/// <summary>
+	/// Always throws InvalidCastException.
+	/// </summary>
+	public Byte ToByte(IFormatProvider? provider) => throw new InvalidCastException();
+
+	/// <summary>
+	/// Always throws InvalidCastException.
+	/// </summary>
+	public Char ToChar(IFormatProvider? provider) => throw new InvalidCastException();
+
+	public DateTime ToDateTime(IFormatProvider? provider) => Convert.ToDateTime(this.Creation, provider);
+
+	public Decimal ToDecimal(IFormatProvider? provider) => this.Id;
+
+	public Double ToDouble(IFormatProvider? provider) => this.Id;
+
+	/// <summary>
+	/// Always throws InvalidCastException.
+	/// </summary>
+	public Int16 ToInt16(IFormatProvider? provider) => throw new InvalidCastException();
+
+	/// <summary>
+	/// Always throws InvalidCastException.
+	/// </summary>
+	public Int32 ToInt32(IFormatProvider? provider) => throw new InvalidCastException();
+
+	public Int64 ToInt64(IFormatProvider? provider) => this.Id;
+
+	/// <summary>
+	/// Always throws InvalidCastException.
+	/// </summary>
+	public SByte ToSByte(IFormatProvider? provider) => throw new InvalidCastException();
+
+	public Single ToSingle(IFormatProvider? provider) => this.Id;
+
+	public String ToString(IFormatProvider? provider) => this.Id.ToString();
+
+	/// <summary>
+	/// Always throws InvalidCastException.
+	/// </summary>
+	public Object ToType(Type conversionType, IFormatProvider? provider) => throw new InvalidCastException();
+
+	/// <summary>
+	/// Always throws InvalidCastException.
+	/// </summary>
+	public UInt16 ToUInt16(IFormatProvider? provider) => throw new InvalidCastException();
+
+	/// <summary>
+	/// Always throws InvalidCastException.
+	/// </summary>
+	public UInt32 ToUInt32(IFormatProvider? provider) => throw new InvalidCastException();
+
+	public UInt64 ToUInt64(IFormatProvider? provider) => Convert.ToUInt64(this.Id, provider);
+
+	/// <summary>
+	/// Returns equality between two snowflakes.
+	/// </summary>
+	public override Boolean Equals(Object? obj)
+		=> obj != null && this.Equals((obj as DiscordSnowflake)!);
+
+	/// <summary>
+	/// Returns the hash code of the snowflake Id
+	/// </summary>
+	public override Int32 GetHashCode()
+		=> this.Id.GetHashCode();
+
+	/// <summary>
+	/// Compares two instances with each other.
+	/// </summary>
+	/// <returns><see langword="true"/> if the two instances have the same ID</returns>
+	public static Boolean operator ==(DiscordSnowflake left, DiscordSnowflake right) => left.Equals(right);
+
+	/// <summary>
+	/// Compares two instances with each other.
+	/// </summary>
+	/// <returns><see langword="true"/> if the two instances have different IDs</returns>
+	public static Boolean operator !=(DiscordSnowflake left, DiscordSnowflake right) => !left.Equals(right);
+}
---
+++ Starnight/Internal/Utils/TimestampConverter.cs
@@ -0,0 +1,77 @@
+namespace Starnight.Internal.Utils;
+
+using Starnight.Internal.Entities;
+
+/// <summary>
+/// Extension method holder to extract data from Snowflakes.
+/// </summary>
+public static class SnowflakeConverter
+{
+	private static readonly DateTimeOffset __discordepoch = new(2015, 1, 1, 0, 0, 0, TimeSpan.Zero);
+
+	/// <summary>
+	/// Extracts the Creation timestamp from a raw snowflake ID.
+	/// </summary>
+	/// <param name="snowflake">A raw snowflake ID as <see cref="Int64"/>.</param>
+	/// <returns>A <see cref="DateTimeOffset"/> holding the requested timestamp.</returns>
+	public static DateTimeOffset GetSnowflakeTime(this Int64 snowflake)
+		=> __discordepoch.AddMilliseconds(snowflake >> 22);
+
+	/// <summary>
+	/// Extracts the Creation timestamp from a wrapped snowflake.
+	/// </summary>
+	/// <param name="snowflake">A wrapped snowflake as <see cref="DiscordSnowflake"/>.</param>
+	/// <returns>A <see cref="DateTimeOffset"/> holding the requested timestamp.</returns>
+	public static DateTimeOffset GetSnowflakeTime(this DiscordSnowflake snowflake)
+		=> __discordepoch.AddMilliseconds(snowflake.Id >> 22);
+
+	/// <summary>
+	/// Extracts the internal worker ID from a raw snowflake ID.
+	/// </summary>
+	/// <param name="snowflake">A raw snowflake ID as <see cref="Int64"/>.</param>
+	/// <returns>A <see cref="Byte"/> holding the internal worker ID as used by Discord.</returns>
+	public static Byte GetInternalWorkerId(this Int64 snowflake)
+		=> (Byte)((snowflake & 0x3E0000) >> 17);
+
+	/// <summary>
+	/// Extracts the internal worker ID from a wrapped snowflake.
+	/// </summary>
+	/// <param name="snowflake">A raw snowflake ID as <see cref="DiscordSnowflake"/>.</param>
+	/// <returns>A <see cref="Byte"/> holding the internal worker ID as used by Discord.</returns>
+	public static Byte GetInternalWorkerId(this DiscordSnowflake snowflake)
+		=> (Byte)((snowflake.Id & 0x3E0000) >> 17);
+
+	/// <summary>
+	/// Extracts the internal process ID from a raw snowflake ID.
+	/// </summary>
+	/// <param name="snowflake">A raw snowflake ID as <see cref="Int64"/>.</param>
+	/// <returns>A <see cref="Byte"/> holding the internal process ID as used by Discord.</returns>
+	public static Byte GetInternalProcessId(this Int64 snowflake)
+		=> (Byte)((snowflake & 0x1F000) >> 12);
+
+	/// <summary>
+	/// Extracts the internal process ID from a wrapped snowflake.
+	/// </summary>
+	/// <param name="snowflake">A raw snowflake ID as <see cref="DiscordSnowflake"/>.</param>
+	/// <returns>A <see cref="Byte"/> holding the internal process ID as used by Discord.</returns>
+	public static Byte GetInternalProcessId(this DiscordSnowflake snowflake)
+		=> (Byte)((snowflake.Id & 0x1F000) >> 12);
+
+	/// <summary>
+	/// Extracts the internal increment from a raw snowflake ID. This number is incremented every time a new ID
+	/// is generated on the process as obtainable from <see cref="GetInternalProcessId(Int64)"/>.
+	/// </summary>
+	/// <param name="snowflake">A raw snowflake ID as <see cref="Int64"/>.</param>
+	/// <returns>A <see cref="Byte"/> holding the internal increment as used by Discord.</returns>
+	public static UInt16 GetIncrement(this Int64 snowflake)
+		=> (UInt16)(snowflake & 0xFFF);
+
+	/// <summary>
+	/// Extracts the internal increment from a wrapped snowflake. This number is incremented every time a new ID
+	/// is generated on the process as obtainable from <see cref="GetInternalProcessId(DiscordSnowflake)"/>.
+	/// </summary>
+	/// <param name="snowflake">A wrapped snowflake as <see cref="DiscordSnowflake"/>.</param>
+	/// <returns>A <see cref="Byte"/> holding the internal increment as used by Discord.</returns>
+	public static UInt16 GetIncrement(this DiscordSnowflake snowflake)
+		=> (UInt16)(snowflake.Id & 0xFFF);
+}
