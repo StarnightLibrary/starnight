165860f156c844bbb7f2c8e5b1073854a171ed3f
ExaInsanity <irisfeanora@outlook.com>
2022-03-20 21:42:12 +0100

    fix converter

--- Starnight/Converters/OptionalParameterJsonConverter.cs
+++ Starnight/Converters/OptionalParameterJsonConverter.cs
@@ -4,12 +4,21 @@ using System;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
-internal class OptionalParameterJsonConverter<TParamType> : JsonConverter<OptionalParameter<TParamType>>
+internal class OptionalParameterJsonConverter<TParamType> : JsonConverter<OptionalParameter<TParamType>?>
 {
-	public override OptionalParameter<TParamType> Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+	public override OptionalParameter<TParamType>? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
 		=> throw new NotImplementedException();
 
 
-	public override void Write(Utf8JsonWriter writer, OptionalParameter<TParamType> value, JsonSerializerOptions options)
-		=> JsonSerializer.Serialize(writer, value.Value, options);
+	public override void Write(Utf8JsonWriter writer, OptionalParameter<TParamType>? value, JsonSerializerOptions options)
+	{
+		if(value == null)
+		{
+			JsonSerializer.Serialize(writer, (Int32?)null, options);
+		}
+		else
+		{
+			JsonSerializer.Serialize(writer, value.Value, options);
+		}
+	}
 }
