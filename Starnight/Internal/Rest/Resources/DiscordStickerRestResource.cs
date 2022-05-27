29c3068827b42e47bc3033d07908314d234a0096
ExaInsanity <irisfeanora@outlook.com>
2022-05-27 15:39:04 +0200

    delete stickers

--- Starnight/Internal/Rest/Resources/DiscordStickerRestResource.cs
+++ Starnight/Internal/Rest/Resources/DiscordStickerRestResource.cs
@@ -4,6 +4,7 @@ using System;
 using System.Buffers;
 using System.Buffers.Text;
 using System.Collections.Generic;
+using System.Net;
 using System.Net.Http;
 using System.Text;
 using System.Text.Json;
@@ -233,5 +234,42 @@ public class DiscordStickerRestResource : AbstractRestResource
 		HttpResponseMessage response = await this.__rest_client.MakeRequestAsync(request);
 
 		return JsonSerializer.Deserialize<DiscordSticker>(await response.Content.ReadAsStringAsync())!;
+	}
+
+	/// <summary>
+	/// Deletes the specified sticker.
+	/// </summary>
+	/// <param name="guildId">Snowflake identifier of the guild owning the sticker.</param>
+	/// <param name="stickerId">Snowflake identifier of the sticker in question.</param>
+	/// <param name="reason">Optional audit log reason.</param>
+	/// <returns>Whether the deletion was successful.</returns>
+	public async ValueTask<Boolean> DeleteGuildStickerAsync
+	(
+		Int64 guildId,
+		Int64 stickerId,
+		String? reason = null
+	)
+	{
+		IRestRequest request = new RestRequest
+		{
+			Path = $"/{Guilds}/{GuildId}/{Stickers}/{StickerId}",
+			Url = new($"{BaseUri}/{Guilds}/{guildId}/{Stickers}/{stickerId}"),
+			Method = HttpMethodEnum.Delete,
+			Headers = reason is not null ? new()
+			{
+				["X-Audit-Log-Reason"] = reason
+			}
+			: new(),
+			Context = new()
+			{
+				["endpoint"] = $"/{Guilds}/{guildId}/{Stickers}/{StickerId}",
+				["cache"] = this.RatelimitBucketCache,
+				["exempt-from-global-limit"] = false
+			}
+		};
+
+		HttpResponseMessage response = await this.__rest_client.MakeRequestAsync(request);
+
+		return response.StatusCode == HttpStatusCode.NoContent;
 	}
 }
