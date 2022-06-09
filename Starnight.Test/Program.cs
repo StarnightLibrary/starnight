98358c6d66780a95888d6b8a2567be87e64ad2c9
ExaInsanity <irisfeanora@outlook.com>
2022-06-09 18:51:34 +0200

    fix startup crash

---
+++ Starnight.Test/Program.cs
@@ -0,0 +1,37 @@
+namespace Starnight.Test;
+
+using System;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using Starnight.Internal.Rest;
+
+internal class Program
+{
+    public static void Main(String[] args)
+    {
+        IServiceCollection collection = new ServiceCollection();
+        _ = collection
+            .AddMemoryCache()
+            .AddLogging();
+
+        try
+        {
+            _ = collection.AddStarnightRestClient(new RestClientOptions()
+            {
+                MedianFirstRequestRetryDelay = TimeSpan.FromSeconds(2),
+                RatelimitedRetryCount = 2,
+                RetryCount = 0,
+                Token = "ksjdlfkjsd"
+            });
+        }
+        catch(Exception e)
+        {
+            Console.WriteLine($"{e}: {e.Message}\n{e.StackTrace}");
+        }
+
+		Console.WriteLine("success");
+
+        _ = Console.ReadKey();
+    }
+}
