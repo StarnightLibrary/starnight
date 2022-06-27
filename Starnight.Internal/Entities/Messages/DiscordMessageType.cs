3dfa5729d6e6dd0b2527efcbf2308d87bc34812a
ExaInsanity <irisfeanora@outlook.com>
2022-06-27 12:22:35 +0200

    update build scripts again

--- Starnight.Internal/Entities/Messages/DiscordMessageType.cs
+++ Starnight.Internal/Entities/Messages/DiscordMessageType.cs
@@ -4,7 +4,7 @@ namespace Starnight.Internal.Entities.Messages;
 /// Represents the type of a message.
 /// </summary>
 /// <remarks>
-/// Documentation for the different fields is missing: where the names are not self explanatory you are on your own.
+/// Documentation for the different fields is missing: where the names are not self-explanatory you are on your own.
 /// Feel free to PR documentation. Additionally, instead of attempting to use more sane names than discord uses,
 /// these names are only changed inasmuch from the discord docs as to conform with the C# naming rules.
 /// </remarks>
