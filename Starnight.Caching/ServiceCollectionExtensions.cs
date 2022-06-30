4c7b455890832d5c3887a4c9f63ec1b5521445b7
ExaInsanity <irisfeanora@outlook.com>
2022-06-30 15:13:20 +0200

    add service collection extensions

---
+++ Starnight.Caching/ServiceCollectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace Starnight.Caching;
+
+using Microsoft.Extensions.DependencyInjection;
+
+using Starnight.Caching.Abstractions;
+using Starnight.Caching.Memory;
+
+/// <summary>
+/// Provides extension methods to <see cref="IServiceCollection"/> for starnight's caching.
+/// </summary>
+public static class ServiceCollectionExtensions
+{
+	/// <summary>
+	/// Adds starnight's memory caching to the service collection.
+	/// </summary>
+	public static IServiceCollection AddStarnightMemoryCache(this IServiceCollection services) 
+		=> services.AddSingleton<ICacheService, MemoryCacheService>();
+}
