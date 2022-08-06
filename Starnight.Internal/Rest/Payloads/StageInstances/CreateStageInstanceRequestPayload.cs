876f44c6956d139e8bb22a5cba5c3464327aa18d
ExaInsanity <irisfeanora@outlook.com>
2022-08-06 17:05:06 +0200

    part 8

--- Starnight.Internal/Rest/Payloads/StageInstances/CreateStageInstanceRequestPayload.cs
+++ Starnight.Internal/Rest/Payloads/StageInstances/CreateStageInstanceRequestPayload.cs
@@ -8,31 +8,29 @@ using Starnight.Internal.Entities.Voice;
 /// <summary>
 /// Represents a payload to POST /stage-instances.
 /// </summary>
-public record CreateStageInstanceRequestPayload
+public sealed record CreateStageInstanceRequestPayload
 {
 	/// <summary>
 	/// The snowflake identifier of the parent stage channel.
 	/// </summary>
-	[JsonNumberHandling(JsonNumberHandling.AllowReadingFromString | JsonNumberHandling.WriteAsString)]
 	[JsonPropertyName("channel_id")]
-	public Int64 ChannelId { get; init; }
+	public required Int64 ChannelId { get; init; }
 
 	/// <summary>
 	/// The topic of the stage instance.
 	/// </summary>
 	[JsonPropertyName("topic")]
-	public String Topic { get; init; } = null!;
+	public required String Topic { get; init; }
 
 	/// <summary>
 	/// The privacy levvel of the stage instance.
 	/// </summary>
-	[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
 	[JsonPropertyName("privacy_level")]
-	public DiscordStagePrivacyLevel? PrivacyLevel { get; init; }
+	public Optional<DiscordStagePrivacyLevel> PrivacyLevel { get; init; }
 
 	/// <summary>
 	/// Specifies whether @everyone should be notified that a stage instance has started.
 	/// </summary>
 	[JsonPropertyName("send_start_notification")]
-	public Boolean? SendStartNotification { get; init; }
+	public Optional<Boolean> SendStartNotification { get; init; }
 }
--- Starnight.Internal/Rest/Payloads/StageInstances/ModifyStageInstanceRequestPayload.cs
+++ Starnight.Internal/Rest/Payloads/StageInstances/ModifyStageInstanceRequestPayload.cs
@@ -8,19 +8,17 @@ using Starnight.Internal.Entities.Voice;
 /// <summary>
 /// Represents a payload to PATCH /stage-instances/:channel_id.
 /// </summary>
-public record ModifyStageInstanceRequestPayload
+public sealed record ModifyStageInstanceRequestPayload
 {
 	/// <summary>
 	/// The topic of the stage instance.
 	/// </summary>
-	[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
 	[JsonPropertyName("topic")]
-	public String? Topic { get; init; }
+	public Optional<String> Topic { get; init; }
 
 	/// <summary>
 	/// The privacy level of the stage instance.
 	/// </summary>
-	[JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
 	[JsonPropertyName("privacy_level")]
-	public DiscordStagePrivacyLevel? PrivacyLevel { get; init; }
+	public Optional<DiscordStagePrivacyLevel> PrivacyLevel { get; init; }
 }
