9f315191105ac1d8b9650417ac5177ff16aeb720
ExaInsanity <irisfeanora@outlook.com>
2022-08-17 19:55:16 +0200

    request guild members

---
+++ Starnight.Internal/Gateway/Payloads/Serverbound/DiscordGatewayRequestGuildMembersCommand.cs
@@ -0,0 +1,25 @@
+namespace Starnight.Internal.Gateway.Payloads.Serverbound;
+
+using System.ComponentModel;
+using System.Runtime.InteropServices;
+using System.Text.Json.Serialization;
+
+using Starnight.Internal.Gateway.Objects.Serverbound;
+
+/// <summary>
+/// Gateway command to request guild members through the gateway.
+/// </summary>
+[StructLayout(LayoutKind.Auto)]
+public record struct DiscordGatewayRequestGuildMembersCommand : IDiscordGatewayPayload<DiscordRequestGuildMembersCommandObject>
+{
+	/// <inheritdoc/>
+	[JsonPropertyName("d")]
+	public required DiscordRequestGuildMembersCommandObject Data { get; init; }
+
+	/// <inheritdoc/>
+	[JsonPropertyName("op")]
+	public DiscordGatewayOpcode Opcode { get; init; } = DiscordGatewayOpcode.RequestGuildMembers;
+
+	[EditorBrowsable(EditorBrowsableState.Never)]
+	public DiscordGatewayRequestGuildMembersCommand() { }
+}
