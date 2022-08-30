1bc9ce8cb186a853a4d5ff03ec3fbc3bc4b1e8de
ExaInsanity <irisfeanora@outlook.com>
2022-08-30 17:54:30 +0200

    channel pins updated

---
+++ Starnight.Internal/Gateway/Objects/Clientbound/Dispatch/DiscordChannelPinsUpdatedEventObject.cs
@@ -0,0 +1,28 @@
+namespace Starnight.Internal.Gateway.Objects.Clientbound.Dispatch;
+
+using System;
+using System.Text.Json.Serialization;
+
+/// <summary>
+/// Represents a payload to a ChannelPinsUpdated event.
+/// </summary>
+public sealed record DiscordChannelPinsUpdatedEventObject
+{
+	/// <summary>
+	/// The ID of the guild.
+	/// </summary>
+	[JsonPropertyName("guild_id")]
+	public Optional<Int64> GuildId { get; init; }
+
+	/// <summary>
+	/// The ID of the channel.
+	/// </summary>
+	[JsonPropertyName("channel_id")]
+	public required Int64 ChannelId { get; init; }
+
+	/// <summary>
+	/// The time at which the most recent pin was pinned.
+	/// </summary>
+	[JsonPropertyName("last_pin_timestamp")]
+	public Optional<DateTimeOffset?> LastPinTimestamp { get; init; }
+}
