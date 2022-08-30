053488062c3a36b2cea1bb27ec92f745171af5b7
ExaInsanity <irisfeanora@outlook.com>
2022-08-30 21:40:34 +0200

    guild ban added

--- Starnight.Internal/Gateway/Payloads/Clientbound/Dispatch/GuildBanAddedPayload.cs
+++ Starnight.Internal/Gateway/Payloads/Clientbound/Dispatch/GuildBanAddedPayload.cs
@@ -1,7 +1,24 @@
 namespace Starnight.Internal.Gateway.Payloads.Clientbound.Dispatch;
+
+using System;
+using System.Text.Json.Serialization;
+
+using Starnight.Internal.Entities.Users;
+
 /// <summary>
-/// Represents the payload of 
+/// Represents the payload of a GuildBanAdded event.
 /// </summary>
-public record struct GuildBanAddedPayload
+public sealed record GuildBanAddedPayload
 {
+    /// <summary>
+    /// ID of the guild this ban was added in.
+    /// </summary>
+    [JsonPropertyName("guild_id")]
+    public required Int64 GuildId { get; init; }
+
+    /// <summary>
+    /// The newly banned user.
+    /// </summary>
+    [JsonPropertyName("user")]
+    public required DiscordUser User { get; init; }
 }
