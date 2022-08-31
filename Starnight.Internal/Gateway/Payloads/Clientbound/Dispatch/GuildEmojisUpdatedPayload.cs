3ceafb18801c703f3fdd0d1148812fda57ff1df2
ExaInsanity <irisfeanora@outlook.com>
2022-08-31 17:33:42 +0200

    guild emojis updated

---
+++ Starnight.Internal/Gateway/Payloads/Clientbound/Dispatch/GuildEmojisUpdatedPayload.cs
@@ -0,0 +1,25 @@
+namespace Starnight.Internal.Gateway.Payloads.Clientbound.Dispatch;
+
+using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+using Starnight.Internal.Entities.Guilds;
+
+/// <summary>
+/// Represents the payload of a GuildEmojisUpdated event.
+/// </summary>
+public sealed record GuildEmojisUpdatedPayload
+{
+	/// <summary>
+	/// Snowflake ID of the guild this is taking place in.
+	/// </summary>
+	[JsonPropertyName("guild_id")]
+	public required Int64 GuildId { get; init; }
+
+	/// <summary>
+	/// The updated emojis.
+	/// </summary>
+	[JsonPropertyName("emojis")]
+	public required IEnumerable<DiscordEmoji> Emojis { get; init; }
+}
