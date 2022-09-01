647749b1ed8f192de675396a210cefe9375501f3
ExaInsanity <irisfeanora@outlook.com>
2022-09-01 18:20:26 +0200

    guild role created

---
+++ Starnight.Internal/Gateway/Payloads/Clientbound/Dispatch/GuildRoleCreatedPayload.cs
@@ -0,0 +1,24 @@
+namespace Starnight.Internal.Gateway.Payloads.Clientbound.Dispatch;
+
+using System;
+using System.Text.Json.Serialization;
+
+using Starnight.Internal.Entities.Guilds;
+
+/// <summary>
+/// Represents the payload for the GuildRoleCreated event.
+/// </summary>
+public sealed record GuildRoleCreatedPayload
+{
+	/// <summary>
+	/// The ID of the guild.
+	/// </summary>
+	[JsonPropertyName("guild_id")]
+	public required Int64 GuildId { get; init; }
+
+	/// <summary>
+	/// The newly created role.
+	/// </summary>
+	[JsonPropertyName("role")]
+	public required DiscordRole Role { get; init; }
+}
