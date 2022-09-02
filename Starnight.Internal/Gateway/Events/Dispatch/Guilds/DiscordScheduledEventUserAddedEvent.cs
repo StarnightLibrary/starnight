86692c4d59d7988be455663083fea8258126a925
ExaInsanity <irisfeanora@outlook.com>
2022-09-02 21:23:12 +0200

    scheduled event user added

---
+++ Starnight.Internal/Gateway/Events/Dispatch/Guilds/DiscordScheduledEventUserAddedEvent.cs
@@ -0,0 +1,28 @@
+namespace Starnight.Internal.Gateway.Events.Dispatch.Guilds;
+
+using System;
+using System.Text.Json.Serialization;
+
+using Starnight.Internal.Gateway.Payloads.Clientbound.Dispatch;
+
+/// <summary>
+/// Represents a GuildScheduledEventUserAdded event.
+/// </summary>
+public sealed record DiscordScheduledEventUserAddedEvent : IDiscordGatewayDispatchPayload<ScheduledEventUserAddedPayload>
+{
+	/// <inheritdoc/>
+	[JsonPropertyName("s")]
+	public required Int32 Sequence { get; init; }
+
+	/// <inheritdoc/>
+	[JsonPropertyName("t")]
+	public required String EventName { get; init; }
+
+	/// <inheritdoc/>
+	[JsonPropertyName("d")]
+	public required ScheduledEventUserAddedPayload Data { get; init; }
+
+	/// <inheritdoc/>
+	[JsonPropertyName("op")]
+	public required DiscordGatewayOpcode Opcode { get; init; }
+}
