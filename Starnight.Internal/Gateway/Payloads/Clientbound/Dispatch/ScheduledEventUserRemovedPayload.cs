283d674d3654af5241ae5ea09fc9d5676edf6810
ExaInsanity <irisfeanora@outlook.com>
2022-09-02 21:25:57 +0200

    scheduled event user removed

---
+++ Starnight.Internal/Gateway/Payloads/Clientbound/Dispatch/ScheduledEventUserRemovedPayload.cs
@@ -0,0 +1,28 @@
+namespace Starnight.Internal.Gateway.Payloads.Clientbound.Dispatch;
+
+using System;
+using System.Text.Json.Serialization;
+
+/// <summary>
+/// Represents the payload for a GuildScheduledEventUserRemoved event.
+/// </summary>
+public sealed record ScheduledEventUserRemovedPayload
+{
+	/// <summary>
+	/// Snowflake ID of the scheduled event.
+	/// </summary>
+	[JsonPropertyName("guild_scheduled_event_id")]
+	public required Int64 ScheduledEventId { get; init; }
+
+	/// <summary>
+	/// Snowflake ID of the user.
+	/// </summary>
+	[JsonPropertyName("user_id")]
+	public required Int64 UserId { get; init; }
+
+	/// <summary>
+	/// Snowflake ID of the guild this took place in.
+	/// </summary>
+	[JsonPropertyName("guild_id")]
+	public required Int64 GuildId { get; init; }
+}
