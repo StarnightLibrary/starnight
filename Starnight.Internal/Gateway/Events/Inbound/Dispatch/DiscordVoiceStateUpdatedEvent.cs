1c80ddf87c9f7aa269e7987193093feef44f5c9c
ExaInsanity <irisfeanora@outlook.com>
2022-09-07 14:06:27 +0200

    voice state updated

---
+++ Starnight.Internal/Gateway/Events/Inbound/Dispatch/DiscordVoiceStateUpdatedEvent.cs
@@ -0,0 +1,28 @@
+namespace Starnight.Internal.Gateway.Events.Inbound.Dispatch;
+
+using System;
+using System.Text.Json.Serialization;
+
+using Starnight.Internal.Entities.Voice;
+
+/// <summary>
+/// Represents a VoiceStateUpdated event.
+/// </summary>
+public sealed record DiscordVoiceStateUpdatedEvent : IDiscordGatewayDispatchPayload<DiscordVoiceState>
+{
+	/// <inheritdoc/>
+	[JsonPropertyName("s")]
+	public required Int32 Sequence { get; init; }
+
+	/// <inheritdoc/>
+	[JsonPropertyName("t")]
+	public required String EventName { get; init; }
+
+	/// <inheritdoc/>
+	[JsonPropertyName("d")]
+	public required DiscordVoiceState Data { get; init; }
+
+	/// <inheritdoc/>
+	[JsonPropertyName("op")]
+	public required DiscordGatewayOpcode Opcode { get; init; }
+}
