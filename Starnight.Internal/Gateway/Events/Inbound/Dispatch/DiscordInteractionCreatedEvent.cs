6c5aec948ce362c2e916debee1f099607d78ffd8
ExaInsanity <irisfeanora@outlook.com>
2022-09-07 14:17:27 +0200

    interaction created

---
+++ Starnight.Internal/Gateway/Events/Inbound/Dispatch/DiscordInteractionCreatedEvent.cs
@@ -0,0 +1,28 @@
+namespace Starnight.Internal.Gateway.Events.Inbound.Dispatch;
+
+using System;
+using System.Text.Json.Serialization;
+
+using Starnight.Internal.Entities.Interactions;
+
+/// <summary>
+/// Represents an InteractionCreated event.
+/// </summary>
+public sealed record DiscordInteractionCreatedEvent : IDiscordGatewayDispatchPayload<DiscordInteraction>
+{
+	/// <inheritdoc/>
+	[JsonPropertyName("s")]
+	public required Int32 Sequence { get; init; }
+
+	/// <inheritdoc/>
+	[JsonPropertyName("t")]
+	public required String EventName { get; init; }
+
+	/// <inheritdoc/>
+	[JsonPropertyName("d")]
+	public required DiscordInteraction Data { get; init; }
+
+	/// <inheritdoc/>
+	[JsonPropertyName("op")]
+	public required DiscordGatewayOpcode Opcode { get; init; }
+}
