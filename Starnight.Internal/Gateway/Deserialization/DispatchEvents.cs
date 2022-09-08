48dc81d7313b38f2dcb343f5357e9f54b768c400
ExaInsanity <irisfeanora@outlook.com>
2022-09-08 16:23:57 +0200

    [ci skip] unused namespace

--- Starnight.Internal/Gateway/Deserialization/DispatchEvents.cs
+++ Starnight.Internal/Gateway/Deserialization/DispatchEvents.cs
@@ -6,7 +6,6 @@ using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
 using System.Reflection.Emit;
-using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Text.Json;
 
