663ebcb3e5c8159466066625706a043bf9415477
ExaInsanity <irisfeanora@outlook.com>
2022-09-30 16:19:29 +0200

    auto-generate guild-related object updates

--- Starnight.Extensions.Caching/Update/Internal/UpdateGuildWrapper.cs
+++ Starnight.Extensions.Caching/Update/Internal/UpdateGuildWrapper.cs
@@ -1,6 +1,8 @@
 namespace Starnight.Extensions.Caching.Update.Internal;
 
 using Starnight.Internal.Entities.Guilds;
+using Starnight.Internal.Entities.Guilds.AutoModeration;
+using Starnight.Internal.Entities.Guilds.Invites;
 using Starnight.SourceGenerators.Caching;
 
 /// <summary>
@@ -27,5 +29,61 @@ internal partial class UpdateGuildWrapper
 	(
 		DiscordGuildIntegration current,
 		DiscordGuildIntegration update
+	);
+
+	[CacheUpdateMethod]
+	public partial DiscordGuildMember UpdateDiscordGuildMember
+	(
+		DiscordGuildMember current,
+		DiscordGuildMember update
+	);
+
+	[CacheUpdateMethod]
+	public partial DiscordGuildPreview UpdateDiscordGuildPreview
+	(
+		DiscordGuildPreview current,
+		DiscordGuildPreview update
+	);
+
+	[CacheUpdateMethod]
+	public partial DiscordGuildTemplate UpdateDiscordGuildTemplate
+	(
+		DiscordGuildTemplate current,
+		DiscordGuildTemplate update
+	);
+
+	[CacheUpdateMethod]
+	public partial DiscordRole UpdateDiscordRole
+	(
+		DiscordRole current,
+		DiscordRole update
+	);
+
+	[CacheUpdateMethod]
+	public partial DiscordScheduledEvent UpdateDiscordScheduledEvent
+	(
+		DiscordScheduledEvent current,
+		DiscordScheduledEvent update
+	);
+
+	[CacheUpdateMethod]
+	public partial DiscordScheduledEventUser UpdateDiscordScheduledEventUser
+	(
+		DiscordScheduledEventUser current,
+		DiscordScheduledEventUser update
+	);
+
+	[CacheUpdateMethod]
+	public partial DiscordInvite UpdateDiscordInvite
+	(
+		DiscordInvite current,
+		DiscordInvite update
+	);
+
+	[CacheUpdateMethod]
+	public partial DiscordAutoModerationRule UpdateDiscordAutoModerationRule
+	(
+		DiscordAutoModerationRule current,
+		DiscordAutoModerationRule update
 	);
 }
