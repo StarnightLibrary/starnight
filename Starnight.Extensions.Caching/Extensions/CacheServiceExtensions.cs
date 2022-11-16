ff1ceb4c8d47ea2beec8a585e633aa18827dbdce
akiraveliara <akiraveliara@outlook.com>
2022-11-16 20:41:44 +0100

    document

--- Starnight.Extensions.Caching/Extensions/CacheServiceExtensions.cs
+++ Starnight.Extensions.Caching/Extensions/CacheServiceExtensions.cs
@@ -9,6 +9,13 @@ using Starnight.Caching.Abstractions;
 
 internal static class CacheServiceExtensions
 {
+	/// <summary>
+	/// Caches an object by the given cache key.
+	/// </summary>
+	/// <typeparam name="T">The type of the object.</typeparam>
+	/// <param name="cache">The ICacheService instance this refers to.</param>
+	/// <param name="object">The object to be cached.</param>
+	/// <param name="cacheKey">The key to cache the object with.</param>
 	public static async ValueTask CacheObjectAsync<T>
 	(
 		this ICacheService cache,
@@ -25,7 +32,17 @@ internal static class CacheServiceExtensions
 		);
 	}
 
-	public static async ValueTask CacheListAsync<T, TParent, TId>
+	/// <summary>
+	/// Caches a list and all its sub-items in the format expected by other components of Starnight.
+	/// </summary>
+	/// <typeparam name="T">The type of the list.</typeparam>
+	/// <typeparam name="TId">The type of the ID used for child items.</typeparam>
+	/// <param name="cache">The ICacheService instance in question.</param>
+	/// <param name="list">The list to be cached.</param>
+	/// <param name="listKey">The key to cache this list under.</param>
+	/// <param name="itemKeyFunction">A function to get the cache key from an individual item from the object.</param>
+	/// <param name="idFunction">A function to get the ID of a list item.</param>
+	public static async ValueTask CacheListAsync<T, TId>
 	(
 		this ICacheService cache,
 		IEnumerable<T> list,
@@ -60,6 +77,15 @@ internal static class CacheServiceExtensions
 		}
 	}
 
+	/// <summary>
+	/// Removes a single item from an existing list.
+	/// </summary>
+	/// <typeparam name="T">The type of the list items.</typeparam>
+	/// <typeparam name="TId">The type of the ID used by the list.</typeparam>
+	/// <param name="cache">The ICacheService instance in question.</param>
+	/// <param name="id">The ID of the item to remove.</param>
+	/// <param name="listKey">The cache key for the list.</param>
+	/// <param name="itemKey">The cache key for the item.</param>
 	public static async ValueTask RemoveListItemAsync<T, TId>
 	(
 		this ICacheService cache,
@@ -87,12 +113,22 @@ internal static class CacheServiceExtensions
 		}
 	}
 
+	/// <summary>
+	/// Adds a single item to a list. If the list does not yet exist, a new one will be created.
+	/// </summary>
+	/// <typeparam name="T">The type of the list items.</typeparam>
+	/// <typeparam name="TId">The type of the ID used by the list.</typeparam>
+	/// <param name="cache">The ICacheService instance in question.</param>
+	/// <param name="item">The item to add to the list.</param>
+	/// <param name="listKey">The cache key used for this list.</param>
+	/// <param name="itemKey">The cache key used for this item.</param>
+	/// <param name="idFunction">A function to get the ID of an item from the item.</param>
 	public static async ValueTask AddListItemAsync<T, TId>
 	(
 		this ICacheService cache,
 		T item,
 		String listKey,
-		Func<T, String> itemKeyFunction,
+		String itemKey,
 		Func<T, TId> idFunction
 	)
 	{
@@ -127,11 +163,19 @@ internal static class CacheServiceExtensions
 
 		await cache.SetAsync
 		(
-			itemKeyFunction(item),
+			itemKey,
 			item
 		);
 	}
 
+	/// <summary>
+	/// Deletes a list and all its child items.
+	/// </summary>
+	/// <typeparam name="T">The type of the list items.</typeparam>
+	/// <typeparam name="TId">The type of the ID used by the list.</typeparam>
+	/// <param name="cache">The IMemoryCache instance in question.</param>
+	/// <param name="listKey">The cache key used for the list.</param>
+	/// <param name="itemKeyFunction">A function to get an item key from an item ID.</param>
 	public static async ValueTask DeleteListAsync<T, TId>
 	(
 		this ICacheService cache,
