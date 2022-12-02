94a3f88d8669e7a42061719dfb6152e5669cdc9c
akiraveliara <akiraveliara@outlook.com>
2022-12-02 18:20:37 +0100

    cache created emojis

--- Starnight.Caching/Shims/CachingEmojiRestResource.Shim.cs
+++ Starnight.Caching/Shims/CachingEmojiRestResource.Shim.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 
+using Starnight.Caching.Services;
 using Starnight.Internal.Entities.Guilds;
 using Starnight.Internal.Rest.Payloads.Emojis;
 using Starnight.Internal.Rest.Resources;
@@ -15,7 +16,47 @@ using Starnight.Internal.Rest.Resources;
 /// </summary>
 public partial class CachingEmojiRestResource : IDiscordEmojiRestResource
 {
-	public ValueTask<DiscordEmoji> CreateGuildEmojiAsync(Int64 guildId, CreateGuildEmojiRequestPayload payload, String? reason = null, CancellationToken ct = default) => throw new NotImplementedException();
+	private readonly IDiscordEmojiRestResource __underlying;
+	private readonly IStarnightCacheService __cache;
+
+	public CachingEmojiRestResource
+	(
+		IDiscordEmojiRestResource underlying,
+		IStarnightCacheService cache
+	)
+	{
+		this.__underlying = underlying;
+		this.__cache = cache;
+	}
+
+	/// <inheritdoc/>
+	public async ValueTask<DiscordEmoji> CreateGuildEmojiAsync
+	(
+		Int64 guildId,
+		CreateGuildEmojiRequestPayload payload,
+		String? reason = null,
+		CancellationToken ct = default
+	)
+	{
+		DiscordEmoji emoji = await this.__underlying.CreateGuildEmojiAsync
+		(
+			guildId,
+			payload,
+			reason,
+			ct
+		);
+
+		await this.__cache.CacheObjectAsync
+		(
+			KeyHelper.GetEmojiKey
+			(
+				emoji.Id!.Value
+			),
+			emoji
+		);
+
+		return emoji;
+	}
 	public ValueTask<Boolean> DeleteGuildEmojiAsync(Int64 guildId, Int64 emojiId, String? reason = null, CancellationToken ct = default) => throw new NotImplementedException();
 	public ValueTask<DiscordEmoji> GetGuildEmojiAsync(Int64 guildId, Int64 emojiId, CancellationToken ct = default) => throw new NotImplementedException();
 	public ValueTask<IEnumerable<DiscordEmoji>> ListGuildEmojisAsync(Int64 guildId, CancellationToken ct = default) => throw new NotImplementedException();
