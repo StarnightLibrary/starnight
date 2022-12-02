5fba4ac659f63ec538ad7f8bdf814e61017f8c54
akiraveliara <akiraveliara@outlook.com>
2022-12-02 20:54:00 +0100

    redirect add guild member role

--- Starnight.Caching/Shims/CachingGuildRestResource.Redirects.cs
+++ Starnight.Caching/Shims/CachingGuildRestResource.Redirects.cs
@@ -1,5 +1,28 @@
 namespace Starnight.Caching.Shims;
 
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
 public partial class CachingGuildRestResource
 {
+	/// <inheritdoc/>
+	public ValueTask<Boolean> AddGuildMemberRoleAsync
+	(
+		Int64 guildId,
+		Int64 userId,
+		Int64 roleId,
+		String? reason = null,
+		CancellationToken ct = default
+	)
+	{
+		return this.underlying.AddGuildMemberRoleAsync
+		(
+			guildId,
+			userId,
+			roleId,
+			reason,
+			ct
+		);
+	}
 }
