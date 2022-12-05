73bde76e3e751fdc387962401a4ed9d201c8adf9
akiraveliara <akiraveliara@outlook.com>
2022-12-05 19:14:30 +0100

    thread list sync

--- Starnight.Caching/Responders/PreChannelResponder.cs
+++ Starnight.Caching/Responders/PreChannelResponder.cs
@@ -3,17 +3,20 @@ namespace Starnight.Caching.Responders;
 using System.Threading.Tasks;
 
 using Starnight.Caching.Services;
+using Starnight.Internal.Entities.Channels;
+using Starnight.Internal.Entities.Channels.Threads;
 using Starnight.Internal.Gateway.Events.Inbound.Dispatch;
 using Starnight.Internal.Gateway.Responders;
 
 /// <summary>
-/// Represents a responder handling incoming channel create/update events.
+/// Represents a responder handling incoming channel-related create/update events.
 /// </summary>
 internal class PreChannelResponder :
 	IResponder<DiscordChannelCreatedEvent>,
 	IResponder<DiscordChannelUpdatedEvent>,
 	IResponder<DiscordThreadCreatedEvent>,
-	IResponder<DiscordThreadUpdatedEvent>
+	IResponder<DiscordThreadUpdatedEvent>,
+	IResponder<DiscordThreadListSyncEvent>
 {
 	private readonly IStarnightCacheService cache;
 
@@ -81,5 +84,36 @@ internal class PreChannelResponder :
 			),
 			@event.Data
 		);
+	}
+
+	public async ValueTask RespondAsync
+	(
+		DiscordThreadListSyncEvent @event
+	)
+	{
+		foreach(DiscordChannel thread in @event.Data.Threads)
+		{
+			await this.cache.CacheObjectAsync
+			(
+				KeyHelper.GetChannelKey
+				(
+					thread.Id
+				),
+				thread
+			);
+		}
+
+		foreach(DiscordThreadMember threadMember in @event.Data.ThreadMemberObjects)
+		{
+			await this.cache.CacheObjectAsync
+			(
+				KeyHelper.GetThreadMemberKey
+				(
+					threadMember.ThreadId,
+					threadMember.UserId
+				),
+				threadMember
+			);
+		}
 	}
 }
