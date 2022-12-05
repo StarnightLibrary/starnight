658900ef565daeb43a1173f6ab06e8b869f78086
akiraveliara <akiraveliara@outlook.com>
2022-12-05 19:07:39 +0100

    add thread caching responders

--- Starnight.Caching/Responders/PreChannelResponder.cs
+++ Starnight.Caching/Responders/PreChannelResponder.cs
@@ -7,11 +7,13 @@ using Starnight.Internal.Gateway.Events.Inbound.Dispatch;
 using Starnight.Internal.Gateway.Responders;
 
 /// <summary>
-/// Represents a responder handling incoming channel events.
+/// Represents a responder handling incoming channel create/update events.
 /// </summary>
 internal class PreChannelResponder :
 	IResponder<DiscordChannelCreatedEvent>,
-	IResponder<DiscordChannelUpdatedEvent>
+	IResponder<DiscordChannelUpdatedEvent>,
+	IResponder<DiscordThreadCreatedEvent>,
+	IResponder<DiscordThreadUpdatedEvent>
 {
 	private readonly IStarnightCacheService cache;
 
@@ -21,7 +23,6 @@ internal class PreChannelResponder :
 	)
 		=> this.cache = cache;
 
-	// caches channels from the channel create event
 	public async ValueTask RespondAsync
 	(
 		DiscordChannelCreatedEvent @event
@@ -40,6 +41,36 @@ internal class PreChannelResponder :
 	public async ValueTask RespondAsync
 	(
 		DiscordChannelUpdatedEvent @event
+	)
+	{
+		await this.cache.CacheObjectAsync
+		(
+			KeyHelper.GetChannelKey
+			(
+				@event.Data.Id
+			),
+			@event.Data
+		);
+	}
+
+	public async ValueTask RespondAsync
+	(
+		DiscordThreadCreatedEvent @event
+	)
+	{
+		await this.cache.CacheObjectAsync
+		(
+			KeyHelper.GetChannelKey
+			(
+				@event.Data.Id
+			),
+			@event.Data
+		);
+	}
+
+	public async ValueTask RespondAsync
+	(
+		DiscordThreadUpdatedEvent @event
 	)
 	{
 		await this.cache.CacheObjectAsync
