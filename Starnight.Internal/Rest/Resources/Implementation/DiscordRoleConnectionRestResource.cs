d18296f3cdf6b5937e797dad4ba31b5b0880c028
akiraveliara <akiraveliara@outlook.com>
2022-12-18 16:24:30 +0100

    update application role connection metadata

--- Starnight.Internal/Rest/Resources/Implementation/DiscordRoleConnectionRestResource.cs
+++ Starnight.Internal/Rest/Resources/Implementation/DiscordRoleConnectionRestResource.cs
@@ -61,5 +61,45 @@ public sealed class DiscordRoleConnectionRestResource
 		)!;
 	}
 
-	public ValueTask<IEnumerable<DiscordRoleConnectionMetadata>> UpdateApplicationRoleConnectionMetadata(System.Int64 applicationId, IEnumerable<DiscordRoleConnectionMetadata> payload, CancellationToken ct = default) => throw new System.NotImplementedException();
+	/// <inheritdoc/>
+	public async ValueTask<IEnumerable<DiscordRoleConnectionMetadata>> UpdateApplicationRoleConnectionMetadata
+	(
+		Int64 applicationId,
+		IEnumerable<DiscordRoleConnectionMetadata> payload,
+		CancellationToken ct = default
+	)
+	{
+		IRestRequest request = new RestRequest
+		{
+			Url = $"{Applications}/{applicationId}/{RoleConnections}/{Metadata}",
+			Method = HttpMethod.Put,
+			Payload = JsonSerializer.Serialize
+			(
+				payload,
+				StarnightInternalConstants.DefaultSerializerOptions
+			),
+			Context = new()
+			{
+				["endpoint"] = $"/{Applications}/{AppId}/{RoleConnections}/{Metadata}",
+				["cache"] = this.RatelimitBucketCache,
+				["exempt-from-global-limit"] = false,
+				["is-webhook-request"] = false
+			}
+		};
+
+		HttpResponseMessage response = await this.restClient.MakeRequestAsync
+		(
+			request,
+			ct
+		);
+
+		return JsonSerializer.Deserialize<IEnumerable<DiscordRoleConnectionMetadata>>
+		(
+			await response.Content.ReadAsStringAsync
+			(
+				ct
+			),
+			StarnightInternalConstants.DefaultSerializerOptions
+		)!;
+	}
 }
