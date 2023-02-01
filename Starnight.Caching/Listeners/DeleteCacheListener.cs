4431ce5f9b492261a3453aa8a9372ebae76ff3f8
akiraveliara <akiraveliara@outlook.com>
2023-02-01 20:10:37 +0100

    handle thread deletions

--- Starnight.Caching/Listeners/DeleteCacheListener.cs
+++ Starnight.Caching/Listeners/DeleteCacheListener.cs
@@ -7,8 +7,10 @@ using Starnight.Internal.Entities.Channels;
 using Starnight.Internal.Gateway.Events.Inbound.Dispatch;
 using Starnight.Internal.Gateway.Listeners;
 
-internal class DeleteCacheListener
-	: IListener<DiscordChannelDeletedEvent>
+internal class DeleteCacheListener :
+	IListener<DiscordChannelDeletedEvent>,
+	IListener<DiscordThreadDeletedEvent>
+	
 {
 	private readonly IStarnightCacheService cache;
 
@@ -21,6 +23,20 @@ internal class DeleteCacheListener
 	public async ValueTask ListenAsync
 	(
 		DiscordChannelDeletedEvent @event
+	)
+	{
+		_ = await this.cache.EvictObjectAsync<DiscordChannel>
+		(
+			KeyHelper.GetChannelKey
+			(
+				@event.Data.Id
+			)
+		);
+	}
+
+	public async ValueTask ListenAsync
+	(
+		DiscordThreadDeletedEvent @event
 	)
 	{
 		_ = await this.cache.EvictObjectAsync<DiscordChannel>
