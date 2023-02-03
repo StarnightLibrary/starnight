f4b234f9e03e9aa14c1d26a4666f139fc2ef4c14
akiraveliara <akiraveliara@outlook.com>
2023-02-03 15:06:41 +0100

    generalize the new workflow script

--- tools/pack-pr-test.sh
+++
@@ -1,4 +0,0 @@
-dotnet pack -o build src/Starnight.Caching.Providers --version-suffix "pr.$1" --include-source --include-symbols
-dotnet pack -o build src/Starnight.Internal --version-suffix "pr.$1" --include-source --include-symbols
-dotnet pack -o build src/Starnight.Caching --version-suffix "pr.$1" --include-source --include-symbols
-dotnet pack -o build src/Starnight --version-suffix "pr.$1" --include-source --include-symbols
