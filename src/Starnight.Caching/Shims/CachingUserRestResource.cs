6a74786360ea5dab6a513560d9c6ee6caaaa6fe4
akiraveliara <akiraveliara@outlook.com>
2023-02-07 18:10:13 +0100

    cache current user guilds

--- src/Starnight.Caching/Shims/CachingUserRestResource.cs
+++ src/Starnight.Caching/Shims/CachingUserRestResource.cs
@@ -51,12 +51,44 @@ public partial class CachingUserRestResource : IDiscordUserRestResource
 
 		return user;
 	}
-	public ValueTask<DiscordGuildMember> GetCurrentUserGuildMemberAsync(Int64 guildId, CancellationToken ct = default) => throw new NotImplementedException();
-	public ValueTask<IEnumerable<DiscordGuild>> GetCurrentUserGuildsAsync(Int64? before = null, Int64? after = null, Int32? limit = null, CancellationToken ct = default) => throw new NotImplementedException();
+
+	public async ValueTask<IEnumerable<DiscordGuild>> GetCurrentUserGuildsAsync
+	(
+		Int64? before = null,
+		Int64? after = null,
+		Int32? limit = null,
+		CancellationToken ct = default
+	)
+	{
+		IEnumerable<DiscordGuild> guilds = await this.underlying.GetCurrentUserGuildsAsync
+		(
+			before,
+			after,
+			limit,
+			ct
+		);
+
+		await Parallel.ForEachAsync
+		(
+			guilds,
+			async (guild, _) =>
+				await this.cache.CacheObjectAsync
+				(
+					KeyHelper.GetGuildKey
+					(
+						guild.Id
+					),
+					guild
+				)
+		);
+
+		return guilds;
+	}
 	public ValueTask<DiscordUser> GetUserAsync(Int64 userId, CancellationToken ct = default) => throw new NotImplementedException();
 
 
 	// redirects
+	public partial ValueTask<DiscordGuildMember> GetCurrentUserGuildMemberAsync(Int64 guildId, CancellationToken ct = default);
 	public partial ValueTask<DiscordChannel> CreateDMAsync(Int64 recipientId, CancellationToken ct = default);
 	public partial ValueTask<IEnumerable<DiscordUserConnection>> GetUserConnectionsAsync(CancellationToken ct = default);
 	public partial ValueTask<Boolean> LeaveGuildAsync(Int64 guildId, CancellationToken ct = default);
